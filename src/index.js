b1cb4ba1bdfced9960364961c62198c631f8e295
nicktheway <nikospa96@hotmail.gr>
2019-10-26 22:40:17 +0100

    Initial text filter implementation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const vm = new Vue({
     el: "#DrawIt",
     data() {
         return {
-            test: 5,
+            textFilter: "",
             wordSets: [],
             url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSj__epm1Cu0PF8OtCIvlFJ3V05d8Qew0QqHX-xPYnpK0XlqoNEPuon6llknJISqcihE4nPP5in8OER/pub?output=csv"
         }
